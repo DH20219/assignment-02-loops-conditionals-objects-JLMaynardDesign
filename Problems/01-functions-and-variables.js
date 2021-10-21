05d987e2ebf274394601643ea6737923ddce2a39
JLMaynardDesign <JLMaynardDesign@gmail.com>
2021-10-21 15:40:21 -0400

    commit

--- Problems/01-functions-and-variables.js
+++ Problems/01-functions-and-variables.js
@@ -71,8 +71,9 @@ function greeting(name) {
 function returnArray (first, second, third) {
   // you can define the array using "new Array ()" or just "[ , , ]"
   // don't forget to return it
-  return [arguments[0], arguments[1], arguments[2]]; 
-}
+  return [arguments[0], arguments[1], arguments[2]]
+}; 
+
 console.log(returnArray(1, 2, 3)); 
 console.log(returnArray('Tim', 'Horton', 'Donuts')); 
 
