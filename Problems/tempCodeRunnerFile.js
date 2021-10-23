3a56113bf3cbcb377aa81f401ff7eb48be5c6740
JLMaynardDesign <JLMaynardDesign@gmail.com>
2021-10-22 23:55:26 -0400

    commit all to submit. Still trying to solve the last problem of Part 1: 02.

--- Problems/tempCodeRunnerFile.js
+++ Problems/tempCodeRunnerFile.js
@@ -1,7 +1,11 @@
-function returnArray (first, second, third) {
-  // you can define the array using "new Array ()" or just "[ , , ]"
-  // don't forget to return it
-  return [arguments[0], arguments[1], arguments[2]]; 
-}
-console.log(returnArray(1, 2, 3)); 
-console.log(returnArray('Tim', 'Horton', 'Donuts')); 
+function sentences(list) {
+  // Hint: "ministers" is an ARRAY of OBJECTS. The simplest way to solve this problem
+  // is to use the "for...of" loop syntax to loop through the array,
+  // and the object[attribute] or object.attribute reference format to access
+  // the internal components of the objects.
+  let result = list.fullName + "\'s reign was " + (list.to - list.from) + "years long." + "\n";
+  for (let i = 0; i < list.length; i++) {
+      const result = list.fullName + "\'s reign was " + (list.to - list.from) + "years long." + "\n";
+      return result;
+    }
+  };
